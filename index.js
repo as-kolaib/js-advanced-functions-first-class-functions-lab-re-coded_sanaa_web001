50ccdf3596628a99619638e79646086592f5a49d
Asma Kolaib <as.kolaib@gmail.com>
2020-08-17 14:29:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers=function(drivers){
+  drivers=[drivers[0],drivers[1]]
+  return drivers
+}
+const returnLastTwoDrivers=function(drivers){
+  drivers=[drivers[drivers.length-2],drivers[drivers.length-1]]
+  return drivers
+}
+const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers];
+
+const createFareMultiplier=function(n){
+    return function(i){
+      if(i){return i * n ;}
+     return n * n ;
+    }
+  }
+  const fareDoubler =(m) => {return createFareMultiplier(m)(2) };
+ 
+  const fareDoubler =(m) => {return createFareMultiplier(m)(3) };
